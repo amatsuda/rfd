00b16be0e278d9b363a9b6dd11f38e746b998159
Akira Matsuda <ronnie@dio.jp>
2013-09-25 01:00:09 +0900

    In the beginning I created the Window and the command

--- lib/rfd.rb
+++ lib/rfd.rb
@@ -1,3 +1,17 @@
 module Rfd
   VERSION = Gem.loaded_specs['rfd'].version.to_s
+
+  class Window
+    def draw(contents)
+      @win.setpos 2, 2
+      @win.addstr contents
+      @win.refresh
+    end
+  end
+
+  class MainWindow < Window
+    def initialize(dir = '.')
+      @win = Curses.stdscr
+    end
+  end
 end

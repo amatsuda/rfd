7c5b38053176aa61bd4460148d4f273b80993b26
Akira Matsuda <ronnie@dio.jp>
2013-10-08 11:33:12 +0900

    Command documentation

--- lib/rfd/commands.rb
+++ lib/rfd/commands.rb
@@ -1,13 +1,16 @@
 module Rfd
   module Commands
+    # Change permission ("A"ttributes) of selected files and directories.
     def a
       process_command_line preset_command: 'chmod'
     end
 
+    # "c"opy selected files and directories.
     def c
       process_command_line preset_command: 'cp'
     end
 
+    # Soft "d"elete (actually mv to the trash folder) selected files and directories.
     def d
       if selected_items.any?
         if ask %Q[Are your sure want to trash #{selected_items.one? ? selected_items.first.name : "these #{selected_items.size} files"}? (y/n)]
@@ -18,22 +21,27 @@ module Rfd
       end
     end
 
+    # Open current file or directory with the "e"ditor
     def e
       edit
     end
 
+    # "f"ind the first flie or directory of which name starts with the given String.
     def f
       process_command_line preset_command: 'find'
     end
 
+    # "f"ind the last flie or directory of which name starts with the given String.
     def F
       process_command_line preset_command: 'find_reverse'
     end
 
+    # Move the cursor to the left pane.
     def h
       (y = current_row - maxy) >= 0 and move_cursor y
     end
 
+    # Move the cursor down.
     def j
       if current_row + 1 >= items.size
         move_cursor 0
@@ -42,6 +50,7 @@ module Rfd
       end
     end
 
+    # Move the cursor up.
     def k
       if current_row == 0
         move_cursor items.size - 1
@@ -50,34 +59,42 @@ module Rfd
       end
     end
 
+    # Move the cursor to the right pane.
     def l
       (y = current_row + maxy) < items.size and move_cursor y
     end
 
+    # "m"ove selected files and directories.
     def m
       process_command_line preset_command: 'mv'
     end
 
+    # "o"pen selected files and directories with the OS "open" command.
     def o
       system "open #{selected_items.map {|i| %Q["#{i.path}"]}.join(' ')}"
     end
 
+    # "q"uit the app.
     def q
       raise StopIteration if ask 'Are your sure want to exit? (y/n)'
     end
 
+    # "s"ort displayed files and directories in the given order.
     def s
       process_command_line preset_command: 'sort'
     end
 
+    # Create a new file, or update its timestamp if the file already exists ("t"ouch).
     def t
       process_command_line preset_command: 'touch'
     end
 
+    # "o"pen selected files and directories with the viewer.
     def v
       view
     end
 
+    # Hard "d"elete selected files and directories.
     def D
       if selected_items.any?
         if ask %Q[Are your sure want to delete #{selected_items.one? ? selected_items.first.name : "these #{selected_items.size} files"}? (y/n)]
@@ -88,22 +105,27 @@ module Rfd
       end
     end
 
+    # Move the cursor to the top.
     def H
       move_cursor current_page * max_items
     end
 
+    # Ma"K"e a directory.
     def K
       process_command_line preset_command: 'mkdir'
     end
 
+    # Move the cursor to the bottom.
     def L
       move_cursor current_page * max_items + displayed_items.size - 1
     end
 
+    # Move the cursor to the "M"iddle.
     def M
       move_cursor current_page * max_items + displayed_items.size / 2
     end
 
+    # Mark or unmark "a"ll files and directories.
     def ctrl_a
       mark = marked_items.size != (items.size - 2)  # exclude . and ..
       items.each {|i| i.toggle_mark unless i.marked? == mark}
@@ -113,18 +135,22 @@ module Rfd
       header_r.wrefresh
     end
 
+    # "b"ack to the previous page.
     def ctrl_b
       ctrl_p
     end
 
+    # "f"orward to the next page.
     def ctrl_f
       ctrl_n
     end
 
+    # Refresh the screen.
     def ctrl_l
       ls
     end
 
+    # Forward to the "n"ext page.
     def ctrl_n
       if total_pages > 1
         if last_page?
@@ -135,6 +161,7 @@ module Rfd
       end
     end
 
+    # Back to the "p"revious page.
     def ctrl_p
       if total_pages > 1
         if first_page?
@@ -145,6 +172,7 @@ module Rfd
       end
     end
 
+    # Change the number of columns in the main window.
     (?1..?9).each do |n|
       define_method(n) do
         spawn_panes n.to_i
@@ -152,22 +180,27 @@ module Rfd
       end
     end
 
+    # Return to the previous directory (popd).
     def -
       popd
     end
 
+    # Search files and directories from the current directory.
     def /
       process_command_line preset_command: 'grep'
     end
 
+    # Execute a shell command in an external shell.
     define_method('!') do
       process_shell_command
     end
 
+    # Execute a command in the controller context.
     define_method(':') do
       process_command_line
     end
 
+    # cd into a directory, or view a file.
     def enter
       if current_item.directory?
         cd current_item
@@ -177,6 +210,7 @@ module Rfd
       end
     end
 
+    # Toggle mark, and move down.
     def space
       toggle_mark
       j
@@ -184,6 +218,7 @@ module Rfd
       header_r.wrefresh
     end
 
+    # cd to the upper hierarchy.
     def del
       if current_dir != '/'
         cd '..'

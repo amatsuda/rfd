de141443c26831905555b7403f7233f22ef15803
Akira Matsuda <ronnie@dio.jp>
2013-10-10 18:46:03 +0900

    Treat enter on . and .. correctly

--- lib/rfd/commands.rb
+++ lib/rfd/commands.rb
@@ -228,7 +228,11 @@ module Rfd
 
     # cd into a directory, or view a file.
     def enter
-      if in_zip?
+      if current_item.name == '.'  # do nothing
+      elsif current_item.name == '..'
+        cd '..'
+        ls
+      elsif in_zip?
         v
       elsif current_item.directory? || current_item.zip?
         cd current_item

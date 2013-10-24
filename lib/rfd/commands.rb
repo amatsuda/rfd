3709a5465b27fb9be052e36a6484bf7165898c26
Akira Matsuda <ronnie@dio.jp>
2013-10-24 12:25:02 +0900

    No need to refresh here

--- lib/rfd/commands.rb
+++ lib/rfd/commands.rb
@@ -192,7 +192,6 @@ module Rfd
       draw_items
       move_cursor current_row
       draw_marked_items
-      header_r.refresh
     end
 
     # "b"ack to the previous page.
@@ -279,7 +278,6 @@ module Rfd
       toggle_mark
       j
       draw_marked_items
-      header_r.refresh
     end
 
     # cd to the upper hierarchy.

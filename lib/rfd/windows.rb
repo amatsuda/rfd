7df098c26b4a924e01b10d0a5bef43dc634768cc
Akira Matsuda <ronnie@dio.jp>
2019-06-13 23:27:06 +0900

    Space Oddity

--- lib/rfd/windows.rb
+++ lib/rfd/windows.rb
@@ -56,12 +56,12 @@ module Rfd
     end
 
     def draw_marked_items(count: 0, size: 0)
-      writeln 0, %Q[#{"#{count}Marked".rjust(11)} #{size.to_s.reverse.gsub( /(\d{3})(?=\d)/, '\1,').reverse.rjust(16)}]
+      writeln 0, %Q[#{"#{count}Marked".rjust(11)} #{size.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse.rjust(16)}]
       noutrefresh
     end
 
     def draw_total_items(count: 0, size: 0)
-      writeln 1, %Q[#{"#{count}Files".rjust(10)} #{size.to_s.reverse.gsub( /(\d{3})(?=\d)/, '\1,').reverse.rjust(17)}]
+      writeln 1, %Q[#{"#{count}Files".rjust(10)} #{size.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse.rjust(17)}]
       noutrefresh
     end
   end

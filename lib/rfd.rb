24cb2f814327611065dc078e149eca7f8e3e0097
Akira Matsuda <ronnie@dio.jp>
2025-12-28 15:34:02 +0900

    :golf:

--- lib/rfd.rb
+++ lib/rfd.rb
@@ -326,34 +326,27 @@ module Rfd
       header_l.draw_path_and_page_number path: current_dir.path, current: current_page + 1, total: total_pages
     end
 
+    SORT_KEYS = {
+      's' => :size, 'S' => :size,
+      't' => :mtime, 'c' => :ctime, 'u' => :atime, 'e' => :extname
+    }.freeze
+
     # Sort the loaded files and directories in already given sort order.
     def sort_items_according_to_current_direction
-      case @direction
-      when nil
-        @items = items.shift(2) + items.partition(&:directory?).flat_map(&:sort)
-      when 'r'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort.reverse}
-      when 'S', 's'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by {|i| -i.size}}
-      when 'Sr', 'sr'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by(&:size)}
-      when 't'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort {|x, y| y.mtime <=> x.mtime}}
-      when 'tr'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by(&:mtime)}
-      when 'c'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort {|x, y| y.ctime <=> x.ctime}}
-      when 'cr'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by(&:ctime)}
-      when 'u'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort {|x, y| y.atime <=> x.atime}}
-      when 'ur'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by(&:atime)}
-      when 'e'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort {|x, y| y.extname <=> x.extname}}
-      when 'er'
-        @items = items.shift(2) + items.partition(&:directory?).flat_map {|arr| arr.sort_by(&:extname)}
+      dots = items.shift(2)
+      reverse = @direction&.end_with?('r')
+      key = SORT_KEYS[@direction&.sub(/r$/, '')]
+
+      sorted = items.partition(&:directory?).flat_map do |arr|
+        if key
+          sorted_arr = arr.sort_by(&key)
+          reverse ? sorted_arr : sorted_arr.reverse
+        else
+          reverse ? arr.sort.reverse : arr.sort
+        end
       end
+
+      @items = dots + sorted
       items.each.with_index {|item, index| item.index = index}
     end
 

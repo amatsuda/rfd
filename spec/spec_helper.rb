1e9c418f07661aa35122b4a4b866714711707e9d
Akira Matsuda <ronnie@dio.jp>
2025-12-31 16:33:29 +0900

    Configure SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_group 'Core', 'lib/rfd.rb'
+  add_group 'Commands', 'lib/rfd/commands.rb'
+  add_group 'File Operations', 'lib/rfd/file_ops.rb'
+  add_group 'Item', 'lib/rfd/item.rb'
+  add_group 'Windows', 'lib/rfd/windows.rb'
+  add_group 'Preview', 'lib/rfd/preview'
+  add_group 'Other' do |src|
+    src.filename.include?('lib/rfd/') &&
+      !src.filename.include?('lib/rfd/preview') &&
+      !src.filename.end_with?('commands.rb') &&
+      !src.filename.end_with?('file_ops.rb') &&
+      !src.filename.end_with?('item.rb') &&
+      !src.filename.end_with?('windows.rb')
+  end
+end
+
 $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 

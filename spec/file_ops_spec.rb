59e6cd8043578619496dd7d9b874d96e6f5cbc83
Akira Matsuda <ronnie@dio.jp>
2025-12-31 17:03:50 +0900

    More tests for file_ops

--- spec/file_ops_spec.rb
+++ spec/file_ops_spec.rb
@@ -6,16 +6,31 @@ describe Rfd::FileOps do
   include_context 'rfd setup'
 
   describe '#cd' do
-    before do
-      controller.cd 'dir1'
-    end
-    its('current_dir.path') { should == File.join(tmpdir, 'dir1') }
-
-    describe '#popd' do
+    context 'to existing directory' do
       before do
-        controller.popd
+        controller.cd 'dir1'
       end
-      its('current_dir.path') { should == tmpdir }
+      its('current_dir.path') { should == File.join(tmpdir, 'dir1') }
+
+      describe '#popd' do
+        before do
+          controller.popd
+        end
+        its('current_dir.path') { should == tmpdir }
+      end
+    end
+
+    context 'to non-existent directory' do
+      it 'returns nil and does not crash' do
+        expect(controller.cd('nonexistent_dir_xyz')).to be_nil
+      end
+    end
+
+    context 'to home directory' do
+      before do
+        controller.cd '~'
+      end
+      its('current_dir.path') { should == File.expand_path('~') }
     end
   end
 
@@ -28,17 +43,56 @@ describe Rfd::FileOps do
   end
 
   describe '#sort' do
-    let(:item) do
-      Dir.mkdir File.join(tmpdir, '.a')
-      stat = File.lstat File.join(tmpdir, '.a')
-      Rfd::Item.new dir: tmpdir, name: '.a', stat: stat, window_width: 100
+    context 'default (by name)' do
+      let(:item) do
+        Dir.mkdir File.join(tmpdir, '.a')
+        stat = File.lstat File.join(tmpdir, '.a')
+        Rfd::Item.new dir: tmpdir, name: '.a', stat: stat, window_width: 100
+      end
+      before do
+        controller.items << item
+        controller.sort
+      end
+      subject { item }
+      its(:index) { should == 2 }  # . .. then next
     end
-    before do
-      controller.items << item
-      controller.sort
+
+    context 'reverse order by name' do
+      before { controller.sort 'r' }
+      it 'sorts directories and files in reverse name order' do
+        dirs = controller.items[2..-1].select(&:directory?)
+        files = controller.items[2..-1].reject(&:directory?)
+        expect(dirs).to eq(dirs.sort.reverse)
+        expect(files).to eq(files.sort.reverse)
+      end
     end
-    subject { item }
-    its(:index) { should == 2 }  # . .. then next
+
+    context 'by size' do
+      before { controller.sort 's' }
+      it 'sorts files by size descending' do
+        files = controller.items[2..-1].reject(&:directory?)
+        sizes = files.map(&:size)
+        expect(sizes).to eq(sizes.sort.reverse)
+      end
+    end
+
+    context 'by mtime' do
+      before { controller.sort 't' }
+      it 'sorts files by mtime descending' do
+        files = controller.items[2..-1].reject(&:directory?)
+        mtimes = files.map(&:mtime)
+        expect(mtimes).to eq(mtimes.sort.reverse)
+      end
+    end
+
+    context 'by extname' do
+      before { controller.sort 'e' }
+      it 'sorts files by extension descending' do
+        files = controller.items[2..-1].reject(&:directory?)
+        extnames = files.map(&:extname)
+        expect(extnames).to eq(extnames.sort.reverse)
+      end
+    end
   end
 
   describe '#chmod' do
@@ -102,13 +156,21 @@ describe Rfd::FileOps do
   end
 
   describe '#grep' do
-    before do
-      controller.grep 'dir'
+    context 'with valid pattern' do
+      before do
+        controller.grep 'dir'
+      end
+      subject { controller.items[2..-1] }
+      its(:size) { should be > 2 }
+      it "all items' name should include 'dir'" do
+        subject.all? {|i| i.name.should include('dir')}
+      end
     end
-    subject { controller.items[2..-1] }
-    its(:size) { should be > 2 }
-    it "all items' name should include 'dir'" do
-      subject.all? {|i| i.name.should include('dir')}
+
+    context 'with invalid regex' do
+      it 'shows error and does not crash' do
+        expect { controller.grep '[invalid' }.not_to raise_error
+      end
     end
   end
 
@@ -132,16 +194,38 @@ describe Rfd::FileOps do
   end
 
   describe '#rename' do
-    before do
-      controller.find '.file2'
-      controller.toggle_mark
-      controller.find 'file3'
-      controller.toggle_mark
-      controller.rename 'fi/faaai'
+    context 'with sed-like pattern' do
+      before do
+        controller.find '.file2'
+        controller.toggle_mark
+        controller.find 'file3'
+        controller.toggle_mark
+        controller.rename 'fi/faaai'
+      end
+      subject { File }
+      it { should be_exist File.join(tmpdir, '.faaaile2') }
+      it { should be_exist File.join(tmpdir, 'faaaile3') }
     end
-    subject { File }
-    it { should be_exist File.join(tmpdir, '.faaaile2') }
-    it { should be_exist File.join(tmpdir, 'faaaile3') }
+
+    context 'with simple new name (no pattern)' do
+      before do
+        controller.find 'file1'
+        controller.rename 'renamed_file'
+      end
+      subject { File }
+      it { should be_exist File.join(tmpdir, 'renamed_file') }
+      it { should_not be_exist File.join(tmpdir, 'file1') }
+    end
+
+    context 'with invalid regex' do
+      before do
+        controller.find 'file1'
+        controller.toggle_mark
+      end
+      it 'shows error and does not crash' do
+        expect { controller.rename '[invalid/replacement' }.not_to raise_error
+      end
+    end
   end
 
   describe '#trash' do
@@ -201,6 +285,17 @@ describe Rfd::FileOps do
     end
     subject { File }
     it { should be_symlink File.join(tmpdir, 'aaa') }
+  end
+
+  describe '#touch_t' do
+    let(:item) { controller.items.detect {|i| !i.directory?} }
+    before do
+      item.toggle_mark
+      controller.touch_t '2020-01-15 12:30:00'
+    end
+    it 'changes the mtime of the file' do
+      expect(File.mtime(item.path)).to eq(Time.parse('2020-01-15 12:30:00'))
+    end
   end
 
   describe '#yank' do
